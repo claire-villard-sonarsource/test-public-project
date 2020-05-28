49d2fc93d98340f01d893985519f899678f3104b
Claire Villard <claire.villard@sonarsource.com>
2020-05-28 18:43:04 +0200

    Fix code smell

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,7 +3,6 @@ package hello;
 public class HelloWorld {
   public static void main(String[] args) {
 	Greeter greeter = new Greeter();
-	var hello = "Hello";
 	System.out.println(greeter.sayHello());
   }
 }
