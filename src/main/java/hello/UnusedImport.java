3264341b1c2201ceff2fdf7ea42b4bfae54a68c6
Claire Villard <claire.villard@sonarsource.com>
2021-11-22 16:44:00 +0100

    Add Unused import class

---
+++ src/main/java/hello/UnusedImport.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2009-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+package hello;
+
+import java.math.BigInteger;
+
+public class UnusedImport {
+
+  private UnusedImport() {
+    // utility class
+  }
+
+  public static boolean isBlank(String nullable) {
+    return nullable == null || nullable.isEmpty() || nullable.matches("\\s+");
+  }
+
+  public static BigInteger getThing() {
+    return BigInteger.valueOf(3);
+  }
+
+}
