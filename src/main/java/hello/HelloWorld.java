2c0a8b3c60012ce774b89b09bb0f4f56418db130
Claire Villard <claire.villard@sonarsource.com>
2024-02-20 10:48:10 +0100

    Suppress issue java:S1481 on HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,7 @@ package hello;
 public class HelloWorld {
   public static void main(String[] args) {
 	Greeter greeter = new Greeter();
+	@SuppressWarnings("java:S1481")
 	var hello = "Hello";
 	System.out.println(greeter.sayHello());
   }
